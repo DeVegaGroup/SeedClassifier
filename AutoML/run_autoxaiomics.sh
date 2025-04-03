531ba6b4390f65406752a3a6450ce3c5f58da8c5
JJ De Vega <2332645+jjdevega@users.noreply.github.com>
2025-04-03 15:24:11 +0100

    Delete AutoML directory

--- AutoML/run_autoxaiomics.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --mem 100G -c32 -p ei-medium
-#SBATCH --mail-type=ALL # notifications for job done & fail
-#SBATCH --mail-user=eloise.barret@earlham.ac.uk #send-to address
-#SBATCH -t 04-00:00 # time (D-HH:MM) 
-
-exec /ei/.project-scratch/e/e88c153a-c2ee-48ea-b637-e8aca3f4d3ca/autoxai4omics_dir/autoxai4omics-1.0.0.img autoxai4omics.sh -m predict -c seed_classifier.json
- 
